13bf636e41d8972cf42b8e1056fc87f40159698c
AtchayaSadasivam <atchayasadasivam30@gmail.com>
2024-07-09 15:56:08 +0530

    completed

--- 02_JS_variables/Ch2_pgm_05.js
+++ 02_JS_variables/Ch2_pgm_05.js
@@ -2,10 +2,13 @@
 
 var message;
 
-message = "Hello World!";
+message = "Hello" + "World!";
 console.log(message);
 
-message = 'Congratulations! Your tweet has won a prize...';
+message = 'Congratulations!'+ ' Your tweet has won' + ' a prize...';
+console.log(message);
+
+message = "Welcome " + "Everyone";
 console.log(message);
 
 
