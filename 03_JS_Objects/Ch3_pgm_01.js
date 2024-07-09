2318841362b1fa48058e065d54646ad98834b9d7
AtchayaSadasivam <atchayasadasivam30@gmail.com>
2024-07-09 20:37:20 +0530

    completed

--- 03_JS_Objects/Ch3_pgm_01.js
+++ 03_JS_Objects/Ch3_pgm_01.js
@@ -8,6 +8,36 @@ bookAuthor = "J. R. R. Tolkien";
 
 console.log(bookTitle + " by " + bookAuthor);
 
+var SecondbookTitle;
+var SecondbookAuthor;
+SecondbookTitle = "Brave New World";
+SecondbookAuthor = "Aldous Huxley";
+console.log(SecondbookTitle + " by " + SecondbookAuthor);
+
+var thirdbookTitle;
+var thirdbookAuthor;
+thirdbookTitle = "Invisible Monsters";
+thirdbookAuthor = "Chuck Palahniuk";
+console.log(thirdbookTitle + " by " + thirdbookAuthor);
+
+var books = [
+    { title: "The Hobbit", author: "J. R. R. Tolkien" },
+    { title: "1984", author: "George Orwell" },
+    { title: "To Kill a Mockingbird", author: "Harper Lee" },
+    { title: "Pride and Prejudice", author: "Jane Austen" },
+    { title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
+    { title: "Moby-Dick", author: "Herman Melville" },
+    { title: "War and Peace", author: "Leo Tolstoy" },
+    { title: "Ulysses", author: "James Joyce" },
+    { title: "The Catcher in the Rye", author: "J.D. Salinger" },
+    { title: "The Lord of the Rings", author: "J. R. R. Tolkien" }
+];
+
+for (var i = 0; i < books.length; i++) {
+    console.log(books[i].title + " by " + books[i].author);
+}
+
+
 
 
 /* Further Adventures
