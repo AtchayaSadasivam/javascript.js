4763955698fd6c54c26d1a28bd092293666df566
AtchayaSadasivam <atchayasadasivam30@gmail.com>
2024-07-10 14:39:11 +0530

    completed

--- 04_JS_Functions/Ch4_pgm_03.js
+++ 04_JS_Functions/Ch4_pgm_03.js
@@ -3,30 +3,34 @@
 var sale1;
 var sale2;
 var sale3;
+var sale4;
 
 sale1 = { price: 140, taxRate: 15 };
 sale2 = { price: 40, taxRate: 10 };
 sale3 = { price: 120, taxRate: 20 };
+sale4 = { price: 240, taxRate: 35 };
 
-sale1.tax = sale1.price * sale1.taxRate / 100;
-sale2.tax = sale2.price * sale2.taxRate / 100;
-sale3.tax = sale3.price * sale3.taxRate / 100;
+function calculateTaxAndTotal(sale) {
+    sale.tax = sale.price * sale.taxRate / 100;
+    sale.total = sale.price + sale.tax;
+}
+calculateTaxAndTotal(sale1);
+calculateTaxAndTotal(sale2);
+calculateTaxAndTotal(sale3);
+calculateTaxAndTotal(sale4);
 
-sale1.total = sale1.price + sale1.tax;
-sale2.total = sale2.price + sale2.tax;
-sale3.total = sale3.price + sale3.tax;
-
-console.log("price = $" + sale1.price);
-console.log("tax @ " + sale1.taxRate + "% = $" + sale1.tax);
-console.log("total cost = $" + sale1.total);
-
-console.log("price = $" + sale2.price);
-console.log("tax @ " + sale2.taxRate + "% = $" + sale2.tax);
-console.log("total cost = $" + sale2.total);
-
-console.log("price = $" + sale3.price);
-console.log("tax @ " + sale3.taxRate + "% = $" + sale3.tax);
-console.log("total cost = $" + sale3.total);
+function displaySaleInfo(sale) {
+    console.log("price = $" + sale.price);
+    console.log("tax @ " + sale.taxRate + "% = $" + sale.tax.toFixed(2));
+    console.log("total cost = $" + sale.total.toFixed(2));
+    console.log("------------------------------\n");
+  }
+  
+  displaySaleInfo(sale1);
+  displaySaleInfo(sale2);
+  displaySaleInfo(sale3);
+  displaySaleInfo(sale4);
+  
 
 
 
