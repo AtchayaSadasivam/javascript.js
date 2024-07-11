f4ef3366ecc264e6fd8fad1383bdca574b6e3ba0
AtchayaSadasivam <atchayasadasivam30@gmail.com>
2024-07-11 19:07:59 +0530

    completed

--- 05_JS_Arguments/Ch5_pgm_09.js
+++ 05_JS_Arguments/Ch5_pgm_09.js
@@ -2,14 +2,19 @@
 
 var showPlayerHealth;
 
+
 showPlayerHealth = function (playerName, playerHealth) {
-    console.log(playerName + " has health " + playerHealth);
+    var healthInfo;
+    healthInfo = playerName + ": health " + playerHealth;
+
+    console.log(healthInfo);
 };
 
 showPlayerHealth("Kandra", 50);
 showPlayerHealth("Dax", 40);
 
-
+showPlayerHealth("Alex",45);
+showPlayerHealth("Binny",78);
 
 /* Further Adventures
  *
