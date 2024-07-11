dfab5caef8a560f182565314dfa751588156d4f0
AtchayaSadasivam <atchayasadasivam30@gmail.com>
2024-07-11 15:20:02 +0530

    completed

--- 05_JS_Arguments/Ch5_pgm_05.js
+++ 05_JS_Arguments/Ch5_pgm_05.js
@@ -1,17 +1,38 @@
 // Using the square function
 
 var square;
+var cube;
+var squareRoot;
 
 square = function (numberToSquare) {
   var result;
   result = numberToSquare * numberToSquare;
   console.log(numberToSquare + " * " + numberToSquare + " = " + result);
 };
+cube = function (numberToCube) {
+  var result;
+  result = numberToCube * numberToCube * numberToCube;
+  console.log(numberToCube + " * "+ numberToCube + " * "+ numberToCube + " = " + result);
+}
+squareRoot = function (number) {
+   var result;
+   result =  Math.sqrt(number);
+   console.log("The squareRoot of" + number + " = " + result);
+}
+
+
 
 square(10);
 square(-2);
 square(1111);
 square(0.5);
+
+cube(2);
+cube(4);
+cube(27);
+cube(12);
+
+squareRoot(9);
 
 
 
