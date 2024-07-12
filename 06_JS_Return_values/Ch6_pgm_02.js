b1611abc0a952f6f9fba6925e6316c80f55ea976
AtchayaSadasivam <atchayasadasivam30@gmail.com>
2024-07-12 15:49:11 +0530

    completed

--- 06_JS_Return_values/Ch6_pgm_02.js
+++ 06_JS_Return_values/Ch6_pgm_02.js
@@ -3,11 +3,12 @@
 var getHelloTo;
 var fullMessage;
 
-getHelloTo = function (name) {
-    return "Hello to " + name;
+getHelloTo = function (name1 , name2) {
+    
+    return "Welcome to our website " + name1+ " " + "and" + " " +name2;
 };
 
-fullMessage = getHelloTo("Kandra");
+fullMessage = getHelloTo("Kandra" , "Das");
 
 console.log(fullMessage);
 
