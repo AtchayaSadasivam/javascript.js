fb16eb61b0f7af0b2336a1eb81ea1744ab29fd13
AtchayaSadasivam <atchayasadasivam30@gmail.com>
2024-07-13 20:34:22 +0530

    completed

--- 07_JS_Object_Arguments/Ch7_pgm_03.js
+++ 07_JS_Object_Arguments/Ch7_pgm_03.js
@@ -9,25 +9,25 @@ var car2;
 var buildcar;
 var getcarinfo;
 
-buildPlanet = function (name, position, type, radius, rank) {
-    return {
-        name: name,
-        position: position,
-        type: type,
-        radius: radius,
-        sizeRank: rank
-    };
-};
+// buildPlanet = function (name, position, type, radius, rank) {
+//     return {
+//         name: name,
+//         position: position,
+//         type: type,
+//         radius: radius,
+//         sizeRank: rank
+//     };
+// };
 
-getPlanetInfo = function (planet) {
-    return planet.name.toUpperCase() + ": planet " + planet.position;
-};
+// getPlanetInfo = function (planet) {
+//     return planet.name.toUpperCase() + ": planet " + planet.position;
+// };
 
-planet1 = buildPlanet("Jupiter", 5, "Gas Giant", 69911, 1);
-planet2 = buildPlanet("Neptune", 8, "Ice Giant", 24622, 4);
+// planet1 = buildPlanet("Jupiter", 5, "Gas Giant", 69911, 1);
+// planet2 = buildPlanet("Neptune", 8, "Ice Giant", 24622, 4);
 
-console.log(getPlanetInfo(planet1));
-console.log(getPlanetInfo(planet2));
+// console.log(getPlanetInfo(planet1));
+// console.log(getPlanetInfo(planet2));
 
 buildcar = function (name, model, type, mileage) {
     return {
@@ -41,8 +41,8 @@ buildcar = function (name, model, type, mileage) {
 getcarinfo = function (car){
     return car.name.toUpperCase() + ": car " + car.model;
 };
-car1 = buildcar("Toyota", "corolla" , "Sedan" , "15000km" );
-car2 = buildcar("Tesla", "Model S" , "Electric" , "5000km");
+car1 = buildcar ("Toyota", "corolla" , "Sedan" , "15000km" );
+car2 = buildcar ("Tesla", "Model S" , "Electric" , "5000km");
 
 console.log(getcarinfo(car1));
 console.log(getcarinfo(car2));
