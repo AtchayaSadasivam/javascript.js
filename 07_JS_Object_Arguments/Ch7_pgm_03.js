fc61d866f3c833ac74c888957043f093c03300ea
AtchayaSadasivam <atchayasadasivam30@gmail.com>
2024-07-13 12:26:09 +0530

    completed

--- 07_JS_Object_Arguments/Ch7_pgm_03.js
+++ 07_JS_Object_Arguments/Ch7_pgm_03.js
@@ -4,6 +4,10 @@ var buildPlanet;
 var getPlanetInfo;
 var planet1;
 var planet2;
+var car1;
+var car2;
+var buildcar;
+var getcarinfo;
 
 buildPlanet = function (name, position, type, radius, rank) {
     return {
@@ -24,6 +28,24 @@ planet2 = buildPlanet("Neptune", 8, "Ice Giant", 24622, 4);
 
 console.log(getPlanetInfo(planet1));
 console.log(getPlanetInfo(planet2));
+
+buildcar = function (name, model, type, mileage) {
+    return {
+        name:name,
+        model:model,
+        type:type,
+        mileage:mileage
+    };
+};
+
+getcarinfo = function (car){
+    return car.name.toUpperCase() + ": car " + car.model;
+};
+car1 = buildcar("Toyota", "corolla" , "Sedan" , "15000km" );
+car2 = buildcar("Tesla", "Model S" , "Electric" , "5000km");
+
+console.log(getcarinfo(car1));
+console.log(getcarinfo(car2));
 
 
 
