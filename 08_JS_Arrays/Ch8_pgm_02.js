6b43187673e6d9956b8a225c54ab9ddcf453f4fe
AtchayaSadasivam <atchayasadasivam30@gmail.com>
2024-07-15 19:37:38 +0530

    completed

--- 08_JS_Arrays/Ch8_pgm_02.js
+++ 08_JS_Arrays/Ch8_pgm_02.js
@@ -5,10 +5,13 @@ var place2 = { name : "The Grand Canyon", country : "USA" };
 var place3 = { name : "Bondi Beach", country : "Australia" };
 
 var thisYear = [ place1, place2 ];
-var nextYear = [ place3 ];
+var nextYear = [ place3, place1 ];
+
+place1.name = " The Great Pyramids";
 
 console.log(thisYear);
 console.log(nextYear);
+
 
 
 
