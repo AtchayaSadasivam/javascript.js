0bfee39ef37c436878c5e812c133d021f9cebce3
AtchayaSadasivam <atchayasadasivam30@gmail.com>
2024-07-15 20:50:22 +0530

    completed

--- 08_JS_Arrays/Ch8_pgm_05.js
+++ 08_JS_Arrays/Ch8_pgm_05.js
@@ -6,7 +6,9 @@ var getVisitorReport = function (visitorArray, dayInWeek) {
     "Tuesday",
     "Wednesday",
     "Thursday",
-    "Friday"
+    "Friday" ,
+    "Saturday" ,
+    "Sunday"
   ];
 	var index = dayInWeek - 1;
 	var visitorReport;
@@ -14,17 +16,40 @@ var getVisitorReport = function (visitorArray, dayInWeek) {
 	visitorReport = "There were ";
   visitorReport += visitorArray[index];
   visitorReport += " visitors ";
-	visitorReport += "on " + days[index];
+	visitorReport += "on " + days[index] + "of week" + week;
 
 	return visitorReport;
 };
 
-var visitors = [ 354, 132, 210, 221, 481 ];
+var visitors = [ 354, 132, 210, 221, 481, 123, 87];
 
-var report = getVisitorReport(visitors, 2);
-
+var report = getVisitorReport(visitors, 5);
 console.log(report);
 
+var reportSaturday = getVisitorReport(visitors, 6);
+console.log(reportSaturday);
+
+var reportSunday = getVisitorReport(visitors, 7);
+console.log(reportSunday);
+
+var week1 = [ 354, 132, 210, 221, 481, 123, 87];
+var week2 = [400, 150, 230, 200, 500, 120, 90];
+var week3 = [360, 140, 220, 210, 470, 110, 80];
+var week4 = [370, 135, 215, 225, 490, 115, 85];
+
+var month = [week1, week2, week3, week4];
+
+var reportWeek1Monday = getMonthVisitorReport(month, 1, 1);
+console.log(reportWeek1Monday); 
+
+var reportWeek2Friday = getMonthVisitorReport(month, 2, 5);
+console.log(reportWeek2Friday); 
+
+var reportWeek3Sunday = getMonthVisitorReport(month, 3, 7);
+console.log(reportWeek3Sunday); 
+
+var reportWeek4Wednesday = getMonthVisitorReport(month, 4, 3);
+console.log(reportWeek4Wednesday); 
 
 
 /* Further Adventures
