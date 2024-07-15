8577be2de1f15e8c8d84bd497aff7e810f2bb335
AtchayaSadasivam <atchayasadasivam30@gmail.com>
2024-07-15 12:30:05 +0530

    completed

--- 07_JS_Object_Arguments/Ch7_pgm_05.js
+++ 07_JS_Object_Arguments/Ch7_pgm_05.js
@@ -7,10 +7,26 @@ var showSmaller = function (num1, num2) {
     console.log(smaller + " is smaller than " + larger);
 };
 
-showSmaller(12, 3);
-showSmaller(-10, 3);
+showSmaller(24, 4);
+showSmaller(-15, 3);
 
+var showLarger = function (num1, num2) {
+    var smaller = Math.min(num1,num2);
+    var larger = Math.max(num1, num2);
 
+    console.log(larger + "is larger than " + smaller);
+}
+showLarger(34,8);
+showLarger(-5, 9);
+
+var showSmallest = function (num1,num2) {
+    var smaller = Math.min(num1,num2);
+    var larger = Math.max(num1, num2);
+
+console.log(larger + "is smallest than " + smaller);
+}
+showSmallest(29,67,98);
+showSmallest(76,-23,24);
 
 /* Further Adventures
  *
